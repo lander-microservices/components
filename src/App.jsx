2e27e75d22dfb5e802c091b6deba92fa4909cc04
Kartik <100852087+cipher997@users.noreply.github.com>
2023-03-18 04:38:28 +0530

    csschanges

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Footer from "./components/Footer1/index";
 import Header from "./components/Header1/index";
+import Testimonials1 from "./components/Testimonials1/index";
 
 import "./index.css";
 
 const App = () => (
   <div className="container">
-    <Header eventID={""} number="(800) 888 99" headerTitle={'QualifyBenefits.co'} />
-    <Footer footerTitle={'QualifyBenefits.co'} fullName="Qualify Benefits" eventID={'Some Event Id'} privacyPolicyRoute={''} termsAndConditionsRoute={''} partnerListRoute={''} />
+    <Header
+      eventID={""}
+      number="(800) 888 99"
+      headerTitle={"QualifyBenefits.co"}
+    />
+    <Testimonials1 />
+    <Footer
+      footerTitle={"QualifyBenefits.co"}
+      fullName="Qualify Benefits"
+      eventID={"Some Event Id"}
+      privacyPolicyRoute={""}
+      termsAndConditionsRoute={""}
+      partnerListRoute={""}
+    />
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
---
+++ src/components/Testimonials1/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./index.scss";
+
+const index = () => {
+  return (
+    <div className="main">
+      <div className="container">
+        <div className="row">
+          <div className="heading">
+            <h1>What Our Customers Are Saying?</h1>
+
+            <div className="paragraph">
+              <p>
+                Here's what some of our thousands of customers over the years
+                have to say!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default index;
