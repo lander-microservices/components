1642830c5a72e33ad13d46402b606032646ed9a3
Pranav Kumar <pranav@wecallmedia.com>
2023-04-06 11:16:05 +0530

    Bug FIxes

--- src/config/apis.js
+++ src/config/apis.js
@@ -1,6 +1,5 @@
 export const GET_IP_ADDRESS = "https://api.ipify.org/?format=json"
-export const PABBLY = "";
 export const SLACK = "";
 
-export const APIS = { GET_IP_ADDRESS, PABBLY, SLACK };
+export const APIS = { GET_IP_ADDRESS, SLACK };
 export default APIS;
